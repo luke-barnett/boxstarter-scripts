89b803b883ad91aa675f8817b4366c8d6a42caa7
Luke Barnett <luke@barnett.net.nz>
2015-03-09 10:36:29 +1300

    Set git ssh to plink

--- essentials.ps1
+++ essentials.ps1
@@ -4,12 +4,13 @@ if($env:BoxStarterScriptsRoot -eq $null)
 }
 
 write-output 'Installing essentials'
+cinst putty
+[Environment]::SetEnvironmentVariable('GIT_SSH', 'plink.exe', 'Machine')
 cinst git
 cinst git-credential-winstore
 cinst atom
 iex ((new-object net.webclient).DownloadString($env:BoxStarterScriptsRoot + 'atom-profile.ps1'))
 iex ((new-object net.webclient).DownloadString($env:BoxStarterScriptsRoot + 'powershell-scripts.ps1'))
 cinst googlechrome
-cinst putty
 cinst vlc
 cinst spotify
