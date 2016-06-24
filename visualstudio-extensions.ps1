a018e1e7ebfed0bcc7c36591b32ab4bb639a368e
Luke Barnett <luke@barnett.net.nz>
2016-06-25 11:24:32 +1200

    Update visualstudio-extensions.ps1

--- visualstudio-extensions.ps1
+++ visualstudio-extensions.ps1
@@ -1,15 +1,17 @@
 write-output 'Installing VS Extensions'
-Install-ChocolateyVsixPackage WebEssentials https://visualstudiogallery.msdn.microsoft.com/ee6e6d8c-c837-41fb-886a-6b50ae2d06a2/file/146119/33/Web%20Essentials%202015.1%20v1.0.198.vsix
+Install-ChocolateyVsixPackage WebExtensionPack https://visualstudiogallery.msdn.microsoft.com/f3b504c6-0095-42f1-a989-51d5fc2a8459/file/186606/20/Web%20Extension%20Pack%20v1.4.44.vsix
 Install-ChocolateyVsixPackage VSCommands https://visualstudiogallery.msdn.microsoft.com/c84be782-b1f1-4f6b-85bb-945ebc852aa1/file/189507/2/SquaredInfinity.VSCommands.VscPackage.vsix.vs14.vsix
 Install-ChocolateyVsixPackage ProductivityPowerTools https://visualstudiogallery.msdn.microsoft.com/34ebc6a2-2777-421d-8914-e29c1dfa7f5d/file/169971/1/ProPowerTools.vsix
 Install-ChocolateyVsixPackage CodeMaid https://visualstudiogallery.msdn.microsoft.com/76293c4d-8c16-4f4a-aee6-21f83a571496/file/9356/33/CodeMaid_v0.9.0.vsix
 Install-ChocolateyVsixPackage IndentGuides https://visualstudiogallery.msdn.microsoft.com/e792686d-542b-474a-8c55-630980e72c30/file/48932/20/IndentGuide%20v14.vsix
 Install-ChocolateyVsixPackage PowershellTools https://visualstudiogallery.msdn.microsoft.com/c9eb3ba8-0c59-4944-9a62-6eee37294597/file/199313/2/PowerShellTools.14.0.vsix
 Install-ChocolateyVsixPackage SpecFlow https://visualstudiogallery.msdn.microsoft.com/c74211e7-cb6e-4dfa-855d-df0ad4a37dd6/file/160542/7/TechTalk.SpecFlow.Vs2015Integration.v2015.1.2.vsix
-Install-ChocolateyVsixPackage OpenCommandLine https://visualstudiogallery.msdn.microsoft.com/4e84e2cf-2d6b-472a-b1e2-b84932511379/file/151803/35/Open%20Command%20Line%20v2.0.168.vsix
 Install-ChocolateyVsixPackage NugetPackageManager https://visualstudiogallery.msdn.microsoft.com/5d345edc-2e2d-4a9c-b73b-d53956dc458d/file/146283/11/NuGet.Tools.vsix
-Install-ChocolateyVsixPackage ImageOptimizer https://visualstudiogallery.msdn.microsoft.com/a56eddd3-d79b-48ac-8c8f-2db06ade77c3/file/38601/40/Image%20Optimizer%20v3.5.84.vsix
 Install-ChocolateyVsixPackage WebAnalyzer https://visualstudiogallery.msdn.microsoft.com/6edc26d4-47d8-4987-82ee-7c820d79be1d/file/181923/23/Web%20Analyzer%20v1.7.75.vsix
-Install-ChocolateyVsixPackage Glyphfriend https://visualstudiogallery.msdn.microsoft.com/5fd24afb-b3b2-4cec-9b03-1cfcec6123aa/file/150806/10/Glyphfriend.vsix
 Install-ChocolateyVsixPackage NUnitTestAdapter https://visualstudiogallery.msdn.microsoft.com/6ab922d0-21c0-4f06-ab5f-4ecd1fe7175d/file/66177/16/NUnitVisualStudioTestAdapter-2.0.0.vsix
 Install-ChocolateyVsixPackage NUnitTestAdapter3 https://visualstudiogallery.msdn.microsoft.com/0da0f6bd-9bb6-4ae3-87a8-537788622f2d/file/158197/10/NUnit3TestAdapter-3.0.9-ctp-9.vsix
+Install-ChocolateyVsixPackage Ignore https://visualstudiogallery.msdn.microsoft.com/d0eba56d-603b-45ab-a680-edfda585f7f3/file/212799/14/ignore%20v1.2.65.vsix
+Install-ChocolateyVsixPackage NPMTaskRunner https://visualstudiogallery.msdn.microsoft.com/8f2f2cbc-4da5-43ba-9de2-c9d08ade4941/file/168331/19/NPM%20Task%20Runner%20v1.3.62.vsix
+Install-ChocolateyVsixPackage OpenInVSCode https://visualstudiogallery.msdn.microsoft.com/33f6f3fd-68e8-4783-b934-ece91a08d265/file/186015/12/Open%20in%20Visual%20Studio%20Code%20v1.3.19.vsix
+Install-ChocolateyVsixPackage DummyTextGenerator https://visualstudiogallery.msdn.microsoft.com/4d809607-87dd-445c-8cd4-585da67c6beb/file/196438/6/Dummy%20Text%20Generator%20v1.1.9.vsix
+Install-ChocolateyVsixPackage TrailingWhitespaceVisualizer https://visualstudiogallery.msdn.microsoft.com/a204e29b-1778-4dae-affd-209bea658a59/file/135653/30/Trailing%20Whitespace%20Visualizer%20v2.4.73.vsix
